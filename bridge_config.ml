db206af845470f0b36f5009d2e8d6989cc80c6f6
Qi LI <liqi0425@gmail.com>
2016-09-12 12:35:34 +0100

    rename and clean

--- bridge_config.ml
+++
@@ -1,7 +0,0 @@
-let config () = [
-    "internal_ip",       "192.168.252.2";
-    "internal_netmask",  "255.255.255.0";
-    "external_ip",       "10.0.0.3";
-    "external_netmask",  "255.255.255.0";
-    "external_gateway",  "10.0.0.1";
-]
