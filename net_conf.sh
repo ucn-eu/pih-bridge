f67ffc66446e1568120bf8361e024f44357679a5
Qi Li <liqi0425@gmail.com>
2016-11-08 17:17:27 +0000

    port forwarding part

--- net_conf.sh
+++ net_conf.sh
@@ -18,4 +18,14 @@ ip addr add $BR1IP dev br1
 ip link set dev br1 up
 
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o wlan0 -s $MASQSRC -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -s $MASQSRC -j MASQUERADE
+
+
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8443 -j DNAT --to 10.0.0.254:8443
+iptables -A FORWARD -p tcp -d 10.0.0.254 --dport 8080 -j ACCEPT
+
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 10003 -j DNAT --to 10.0.0.2:10003
+iptables -A FORWARD -p tcp -d 10.0.0.2 --dport 10003 -j ACCEPT
+
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 10004 -j DNAT --to 10.0.0.2:10004
+iptables -A FORWARD -p tcp -d 10.0.0.2 --dport 10004 -j ACCEPT
